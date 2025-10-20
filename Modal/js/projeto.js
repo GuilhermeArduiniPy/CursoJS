090087695b34ff1f14d79711832f1273df810c92
GuilhermeArduiniPy <guilhermeardn1@hotmail.com>
2025-10-20 10:47:02 -0300

    updates projeto e curso

---
+++ Modal/js/projeto.js
@@ -0,0 +1,11 @@
+import InitScrollSsuave from "./modules/scroll-suave.js";
+import initAniamcaoScroll from "./modules/scroll-animacao.js";
+import iniAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnavegacao.js";
+import initModal from "./modules/modal.js";
+
+initModal();
+InitScrollSsuave();
+initAniamcaoScroll();
+iniAccordion();
+initTabNav();
---
+++ modules-introducao/js/projeto.js
@@ -0,0 +1,9 @@
+import InitScrollSsuave from "./modules/scroll-suave.js";
+import initAniamcaoScroll from "./modules/scroll-animacao.js";
+import iniAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnavegacao.js";
+
+InitScrollSsuave();
+initAniamcaoScroll();
+iniAccordion();
+initTabNav();
