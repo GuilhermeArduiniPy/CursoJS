93f4e18fb709a82200208e6f39cffdb6f4650fc5
GuilhermeArduiniPy <guilhermeardn1@hotmail.com>
2025-10-21 17:12:25 -0300

    Andamento cursoJS

---
+++ ToolTip/js/projeto.js
@@ -0,0 +1,15 @@
+import InitScrollSsuave from "./modules/scroll-suave.js";
+import initAniamcaoScroll from "./modules/scroll-animacao.js";
+import iniAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnavegacao.js";
+import initModal from "./modules/modal.js";
+import initTolltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initDropdownMenu();
+initTolltip();
+initModal();
+InitScrollSsuave();
+initAniamcaoScroll();
+iniAccordion();
+initTabNav();
---
+++ setTimeOut/setTimeOut.js
@@ -0,0 +1,22 @@
+// function espera(texto) {
+//   console.log(texto);
+// }
+
+// setTimeout(espera, 2000, "depois de 2 segundos");
+
+// function loop(texto) {
+//   console.log(texto);
+// }
+
+// let i = 0;
+
+// const meuLoop = setInterval(() => {
+//   console.log(i++);
+//   if (i > 10) {
+//     clearInterval(meuLoop);
+//   }
+// }, 300);
+
+//Exercicios
+//Mude a cor da tela para azul e depois para vermelho a cada 2s.
+//Crie um cronometro utilizando o setInterval. Deve ser possivel iniciar, pausar e resetar (duplo clique no pausar)
